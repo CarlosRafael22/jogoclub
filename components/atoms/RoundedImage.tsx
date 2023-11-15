39caf4ae4f68648ce42d3653405d854a255055e6
Rafael Leitao <carlosrafael093@gmail.com>
2023-11-15 23:01:11 +0100

    feat(atoms): Add atoms components

---
+++ components/atoms/RoundedImage.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const RoundedImage = styled.img`
+    border-radius: 50%;
+    width: 90px;
+    height: 90px;
+`
+export default RoundedImage
---
+++ components/atoms/SocialMediaLink.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const SocialMediaLink = styled.a<{ $logo: string }>`
+    background-image: url(${props => props.$logo});
+    display: block;
+    margin: 0 4px;
+    text-indent: -9999px;
+    width: 50px;
+    height: 50px;
+`
+
+export const SOCIAL_MEDIA_LOGO_MAPPING = {
+    youtube: "/assets/social_medias/icons8-youtube-50.png",
+    instagram: "/assets/social_medias/icons8-instagram-50.png",
+    tiktok: "/assets/social_medias/icons8-tiktok-50.png"
+}
+
+export default SocialMediaLink
