59ae6433d339979ddc61d13e718b410df11ba85c
Rafael Leitao <carlosrafael093@gmail.com>
2024-09-22 01:05:26 +0200

    feat(MatchScore): Adapt prop type + change style

--- components/molecules/MatchScore/index.tsx
+++ components/molecules/MatchScore/index.tsx
@@ -1,15 +1,9 @@
 import TeamNameBadge from "../TeamNameBadge";
-import { type TeamScoreInfo } from "entities/models/team";
-import { type ScoreInfo } from "entities/models/match";
+import { type BasicMatchInfo } from "entities/models/match";
 import Typography from "@/atoms/Typography";
 import { getFormattedDateAndHourFromDateString, getHourText } from "helpers";
 
-type Props = {
-    homeTeam: TeamScoreInfo;
-    awayTeam: TeamScoreInfo;
-    kickoff: string;
-    scoreInfo: ScoreInfo;
-}
+type Props = Omit<BasicMatchInfo, "competition" | "stadium">;
 
 const MatchScore = ({ homeTeam, awayTeam, kickoff, scoreInfo }: Props) => {
     const { name: homeTeamName, code: homeTeamCode, logoUrl: homeTeamLogoUrl } = homeTeam;
@@ -20,7 +14,7 @@ const MatchScore = ({ homeTeam, awayTeam, kickoff, scoreInfo }: Props) => {
     const dateTimeInfo = scoreInfo ? `${formattedDate} - ${getHourText(formattedHour)}` : formattedDate;
     
     return (
-        <div className="flex flex-col gap-1 items-center">
+        <div className="flex flex-col gap-2 items-center">
             <div className="flex flex-row gap-1 items-center">
                 <TeamNameBadge name={homeTeamName} code={homeTeamCode} logoUrl={homeTeamLogoUrl} isHomeTeam={true} />
                 <div className="bg-gray-300 px-2 py-1 rounded flex items-center h-fit mx-1">
@@ -28,7 +22,7 @@ const MatchScore = ({ homeTeam, awayTeam, kickoff, scoreInfo }: Props) => {
                 </div>
                 <TeamNameBadge name={awayTeamName} code={awayTeamCode} logoUrl={awayTeamLogoUrl} />
             </div>
-            <Typography variant="body2-bold">{dateTimeInfo}</Typography>
+            <p className="text-sm font-bold">{dateTimeInfo}</p>
         </div>
     );
 };
